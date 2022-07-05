32ce4a37f8bf17e5086a271099c45782075b2410
SGKwon95 <ssunder7@naver.com>
2022-07-06 02:37:05 +0900

    Initial commit

---
+++ src/main/java/com/example/boardbasic/BoardbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardbasicApplication.class, args);
+    }
+
+}
